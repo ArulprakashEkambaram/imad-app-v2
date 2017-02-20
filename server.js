06b1aef4ab9cbc312ccade07d51ef19a0b7f71af
ArulprakashEkambaram <arulprakash.ekam@gmail.com>
2017-02-21 00:37:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/Mist0ne', function (req, res) {
+app.get('/Mistone', function (req, res) {
   res.send(`mist one Arulprakash is displayed`);
 });
 
