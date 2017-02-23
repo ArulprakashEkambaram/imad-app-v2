2509be5b97413faf16ad46bc1956ca00f1862d05
ArulprakashEkambaram <arulprakash.ekam@gmail.com>
2017-02-23 20:26:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,15 @@ var element = document.getElementById('main-text'
 );
 
 Element.innerHTML = "I have changed the content" ;
+
+// move the image
+var img = document.getElementByID('prakash'
+);
+var marginleft=0;
+function moveright() {
+    marginleft= marginleft+10;
+    img.style.marginleft+'px';
+}
+img.onclick= function(){
+    var interval= setinterval(moveright,100);
+}
