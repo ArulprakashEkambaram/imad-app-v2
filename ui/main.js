7f5be3f59b37d47734d4a3b66bf613a71820fbb0
ArulprakashEkambaram <arulprakash.ekam@gmail.com>
2017-03-07 15:57:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,14 @@
-console.log('Loaded!');
-
-//change the text of the main-text div
-var element = document.getElementById('main-text'
-);
-
-Element.innerHTML = "I have changed the content" ;
-
-// move the image
-var img = document.getElementByID('prakash'
-);
-var marginleft=0;
-function moveright() {
-    marginleft= marginleft+10;
-    img.style.marginleft+'px';
-}
-img.onclick= function(){
-    var interval= setinterval(moveright,100);
-}
+//counter code
+var buttton =document.getById('counter');
+var counter = 0;
+button.onclick = function(){
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in a variable 
+    
+    //Render the variable in the correct span
+    counter = counter+1;
+    var span= document.getElementById('count');
+    span.innerHTML= counter.toString();
+    
+};
